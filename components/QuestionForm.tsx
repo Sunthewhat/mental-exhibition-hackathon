c3058dc1bc5c28b0c1eeec4a9e7b865c2d1496f6
sorasora46 <sorrawit02546@gmail.com>
2024-03-19 16:43:48 +0700

    Add progress bar from shadcn/ui

--- components/QuestionForm.tsx
+++ components/QuestionForm.tsx
@@ -2,6 +2,7 @@ import questions from "@/constants/questions";
 import useMultiStepsForm from "@/hooks/useMultiStepsForm";
 import Question from "./Question";
 import { useRouter } from "next/navigation";
+import { Progress } from "./ui/progress";
 
 interface QuestionFormProps {
   setEnterGame: (value: boolean) => void;
@@ -46,19 +47,37 @@ const QuestionForm: React.FC<QuestionFormProps> = ({
         <p>MENTAL HEALTH</p>
         <p>EXHIBITION</p>
       </div>
-      <div className="pb-8 w-full w-3/4 md:w-2/3 lg:w-1/3 z-10 text-center font-bold text-md md:text-base lg:text-lg bg-white rounded-t-[200px] rounded-b-[15px]">
+      <div className="pb-8 w-full md:w-1/2 lg:w-1/3 z-10 text-center font-bold text-md md:text-base lg:text-lg bg-white rounded-t-[200px] rounded-b-[15px]">
         <form>{step}</form>
       </div>
-      <div className="z-10 flex gap-3">
-        <div>
-          {currentStepIndex + 1}/{steps.length}
+      <div className="pb-8 z-10 flex flex-col items-center justify-center gap-3 w-full">
+        <div className="lg:w-1/3 md:w-1/2 w-full flex flex-col items-center justify-center">
+          <Progress
+            value={((currentStepIndex + 1) / steps.length) * 100}
+            className="h-2 border border-rose-300"
+          />
+          <p className="self-start text-xs mt-2">
+            {currentStepIndex + 1}/{steps.length}
+          </p>
         </div>
-        <button type="button" onClick={handleClickBack}>
-          back
-        </button>
-        <button type="button" onClick={handleClickNext}>
-          next
-        </button>
+        <div className="flex justtify-center items-center gap-8">
+          <button type="button" onClick={handleClickBack}>
+            <div
+              className="w-0 h-0 
+  border-t-[25px] border-t-transparent
+  border-r-[30px] border-r-rose-300
+  border-b-[25px] border-b-transparent"
+            ></div>
+          </button>
+          <button type="button" onClick={handleClickNext}>
+            <div
+              className="w-0 h-0 
+  border-t-[25px] border-t-transparent
+  border-l-[30px] border-l-rose-300
+  border-b-[25px] border-b-transparent"
+            ></div>
+          </button>
+        </div>
       </div>
     </div>
   );
