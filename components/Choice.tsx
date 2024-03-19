eb3e06767a1a146c0a1d038cdb85ec057719b406
sorasora46 <sorrawit02546@gmail.com>
2024-03-19 15:59:22 +0700

    Create Choice.tsx

---
+++ components/Choice.tsx
@@ -0,0 +1,45 @@
+interface ChoiceProp {
+  id: string;
+  name: string;
+  value: string;
+  onClick: (choice: number) => void;
+  checked: boolean;
+  choice: string;
+}
+
+const Choice: React.FC<ChoiceProp> = ({
+  id,
+  name,
+  value,
+  onClick,
+  checked,
+  choice,
+}) => {
+  const handleChoiceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    onClick(Number(event.currentTarget.value));
+  };
+
+  return (
+    <div
+      className={`cursor-pointer py-5 px-32 border-2 rounded-2xl w-full ${
+        checked ? "border-amber-300" : "border-red-300"
+      }`}
+      onClick={() => onClick(Number(value))}
+    >
+      <input
+        className="appearance-none"
+        type="radio"
+        id={id}
+        name={name}
+        value={value}
+        onChange={handleChoiceChange}
+        checked={checked}
+      />
+      <label htmlFor={id} key={value} className="cursor-pointer">
+        {choice}
+      </label>
+    </div>
+  );
+};
+
+export default Choice;
