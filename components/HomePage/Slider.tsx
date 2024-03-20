71f4372c996f70d2cfe132d6b1e80a25c3d69e56
Boolin J <boolin.work@gmail.com>
2024-03-20 19:47:07 +0700

    initial commit of homepage

---
+++ components/HomePage/Slider.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import "swiper/css";
+import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from "swiper/react";
+import Image from "next/image";
+
+export default function Slider() {
+
+  return (
+    <>
+        <Swiper 
+          spaceBetween={10}
+          slidesPerView={1}
+          loop={true}
+          className="rounded-3xl w-full h-[250px] md:h-[340px] lg:h-[480px] shadow-md"
+        >
+          <SwiperSlide><Image src='/banner.png' alt='banner' quality={100} fill/></SwiperSlide>
+          <SwiperSlide><Image src='/banner.png' alt='banner' quality={100} fill/></SwiperSlide>
+          <SwiperSlide><Image src='/banner.png' alt='banner' quality={100} fill/></SwiperSlide>
+          <SwiperSlide><Image src='/banner.png' alt='banner' quality={100} fill/></SwiperSlide>
+        </Swiper>
+    </>
+  );
+}
