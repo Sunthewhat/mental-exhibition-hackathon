24d32aaa1764827fddb6528c96245658d6ea5529
Boolin J <boolin.work@gmail.com>
2024-03-23 13:53:54 +0700

    Minor fixs
    /page.module.css
     - adjust padding

--- components/Navigation/NavigationBar.tsx
+++ components/Navigation/NavigationBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Menu } from "lucide-react";
-import { Noto_Sans_Thai } from "next/font/google";
+import { Noto_Sans_Thai, Noto_Sans } from "next/font/google";
 import Link from "next/link";
 import { ReactNode, useState } from "react";
 import Sidebar from "./Sidebar";
@@ -10,14 +10,15 @@ interface Props {
   children: ReactNode;
 }
 
-const noto = Noto_Sans_Thai({ subsets: ["latin"] });
+const notoThai = Noto_Sans_Thai({ subsets: ["latin"] });
+const noto = Noto_Sans({ subsets: ["latin"] });
 
 export default function NavigationBar({ children }: Props) {
   const [showSidebar, setShowSidebar] = useState<boolean>();
 
   return (
     <>
-      <div className="px-[60px] flex justify-between">
+      <div className="px-[30px] md:px-[60px] flex justify-between">
         <div className="flex justify-between gap-2">
           <Menu
             color="#fff"
@@ -26,16 +27,18 @@ export default function NavigationBar({ children }: Props) {
               setShowSidebar((prev) => !prev);
             }}
           />
-          <Link href="/" className={`${noto.className} text-white font-bold`}>
+          <Link
+            href="/"
+            className={`${noto.className} text-base md:text-lg text-white font-bold`}
+          >
             Mental Health Exhibition & Hackathon 2024
           </Link>
-
         </div>
 
         <Sidebar state={showSidebar} setState={setShowSidebar} />
 
         <div
-          className={`${noto.className} text-sm text-white hidden lg:flex justify-between gap-[90px]`}
+          className={`${notoThai.className} text-sm text-white hidden lg:flex items-center justify-between gap-[90px]`}
         >
           <Link href="/">Home</Link>
           <Link href="/">Hackathon</Link>
