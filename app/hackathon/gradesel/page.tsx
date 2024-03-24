25cd2059db946e680224c25495d094bf4f7b7ecb
Charana Sukroat <97074318+lnwwaveza007@users.noreply.github.com>
2024-03-24 19:45:41 +0700

    Fix summit (Game) and add navbar to form

--- app/hackathon/gradesel/page.tsx
+++ app/hackathon/gradesel/page.tsx
@@ -3,7 +3,7 @@ import GradeBox from "@/features/hackathon/components/boxs/GradeBox";
 export default function GradeSelectPage() {
   return (
     <>
-      <GradeBox />;
+      <GradeBox />
     </>
   );
 }
