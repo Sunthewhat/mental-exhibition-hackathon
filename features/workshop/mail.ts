47363d044a37b9f26287d69869cfa556ded72281
SukumDecha <ninemaster12gt@gmail.com>
2024-04-16 23:30:41 +0700

    remove email route

--- features/workshop/mail.ts
+++ features/workshop/mail.ts
@@ -1,23 +1,53 @@
-"use client";
+"use server";
 
-import { ReserveConfirmationProps } from "./components/ReserveConfirmation";
+import { render } from "@react-email/render";
+import nodemailer from "nodemailer";
 
-export const assertSendEmail = async (data: ReserveConfirmationProps) => {
-  if (!data.email) {
+import {
+  ReserveConfirmation,
+  ReserveConfirmationProps,
+} from "./components/ReserveConfirmation";
+import React from "react";
+import { getLocByWorkshop } from "./helper";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "charana.sukr@mail.kmutt.ac.th",
+    pass: "wsak vsfc pyyg atol",
+  },
+});
+
+export const assertSendEmail = async ({
+  userName,
+  workShop,
+  date,
+  email,
+}: ReserveConfirmationProps) => {
+  if (!email) {
     throw Error("A target email is required.");
   }
 
+  const location = getLocByWorkshop(workShop);
   try {
-    await fetch("/api/workshop/mail", {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+    const options = {
+      // from: "Mental Exhibition <mentalexhibition@hackmindgallery-kmutt.com>",
+      from: "[Mental Health Exhibition & Hackathon] <mentalexhibition@mail.kmutt.ac.th>",
+      to: email,
+      subject: "[reservation confirmed] ยืนยันการจองเวิร์คช็อป",
+      html: render(
+        React.createElement(ReserveConfirmation, {
+          userName,
+          workShop,
+          date,
+          location,
+        })
+      ),
+    };
 
-    console.log(`Email (${data.workShop}) has been sent to user.`);
+    await transporter.sendMail(options);
+    console.log(`Email (${workShop}) has been sent to user.`);
   } catch (err) {
-    console.log(`Error sending email (${data.workShop}) `);
+    console.log(`Error sending email (${workShop}) `);
   }
 };
