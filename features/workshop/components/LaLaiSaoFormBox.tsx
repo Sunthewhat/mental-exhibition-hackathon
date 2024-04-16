29a37f70b2d0ee3673afbc2e2160354dfae7e081
Charana Sukroat <97074318+lnwwaveza007@users.noreply.github.com>
2024-04-17 00:45:26 +0700

    Please I beg you

--- features/workshop/components/LaLaiSaoFormBox.tsx
+++ features/workshop/components/LaLaiSaoFormBox.tsx
@@ -154,12 +154,12 @@ const LaLaiSaoBox = ({
         await updateRegisterCount("LaLaiSao", date as string)
           .catch((error) => console.error("Error updating user count:", error));
 
-        await assertSendEmail({
-          userName: fullname as string,
-          workShop: "LaLaiSao",
-          date: date as string,
-          email,
-        });
+        const emailForm = new FormData();
+        emailForm.set("userName", fullname as string);
+        emailForm.set("workShop", "MatchaMoments");
+        emailForm.set("date", date as string);
+        emailForm.set("email", email);
+        await assertSendEmail(emailForm);
 
         router.push(`/workshop/${link}/submit`);
       } else {
